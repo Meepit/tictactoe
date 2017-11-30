57ac8d9997581af07d5339de90634e47d04ad425
James Lizamore <jwlizamore@gmail.com>
2017-11-30 20:46:57 +0000

    Added place piece method

--- lib/board.rb
+++ lib/board.rb
@@ -3,4 +3,15 @@ class Board
   def initialize
     @state = [['#', '#', '#'],['#', '#', '#'],['#', '#', '#']]
   end
+
+  def place_piece(piece, position)
+    row, column = get_row_rolumn(position)
+    state[row][column] = piece
+  end
+
+  private
+
+  def get_row_rolumn(position)
+    [position / 3, position % 3]
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,4 +4,8 @@ describe Board do
   it 'should be initialized with an empty board state' do
     expect(subject.state).to eq [['#', '#', '#'],['#', '#', '#'],['#', '#', '#']]
   end
+  it 'should change the first tile to an X' do
+    subject.place_piece('X', 0)
+    expect(subject.state).to eq [['X', '#', '#'],['#', '#', '#'],['#', '#', '#']]
+  end
 end
