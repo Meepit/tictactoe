99c70b2c57d47706eae42f5adc9035a4ff63b50d
James Lizamore <jwlizamore@gmail.com>
2017-11-30 21:16:58 +0000

    Added get_tile method

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,30 @@
 class Board
   attr_reader :state
   def initialize
-    @state = [['#', '#', '#'],['#', '#', '#'],['#', '#', '#']]
+    @state = [['0', '1', '2'],['3', '4', '5'],['6', '7', '8']]
   end
 
   def place_piece(piece, position)
     row, column = get_row_rolumn(position)
-    state[row][column] = piece
+    modify_state(piece, position)
+  end
+
+  def get_tile_state(position)
+    ('012345678'.include? get_tile(position)) ? "empty" : get_tile(position)
   end
 
   private
-
   def get_row_rolumn(position)
     [position / 3, position % 3]
+  end
+
+  def modify_state(piece, position)
+    row, column = get_row_rolumn(position)
+    state[row][column] = piece
+  end
+
+  def get_tile(position)
+    row, column = get_row_rolumn(position)
+    state[row][column]
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,10 +2,13 @@ require 'board'
 
 describe Board do
   it 'should be initialized with an empty board state' do
-    expect(subject.state).to eq [['#', '#', '#'],['#', '#', '#'],['#', '#', '#']]
+    expect(subject.state).to eq [['0', '1', '2'],['3', '4', '5'],['6', '7', '8']]
   end
   it 'should change the first tile to an X' do
     subject.place_piece('X', 0)
-    expect(subject.state).to eq [['X', '#', '#'],['#', '#', '#'],['#', '#', '#']]
+    expect(subject.state).to eq [['X', '1', '2'],['3', '4', '5'],['6', '7', '8']]
+  end
+  it 'should return empty when getting empty tile' do
+    expect(subject.get_tile_state(3)).to eq "empty"
   end
 end
